2622696ffb7ebc06d5232d438de1c62db45e9439
JmanCentral <jmancipet@ucentral.edu.co>
2025-09-15 19:00:38 -0500

    deploy

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'puntoventa'); // Cambia 'tu_base_de_datos' por el nombre de tu base de datos
+define('DB_HOST', 'sql112.byethost15.com');
+define('DB_USER', 'b15_39936194');
+define('DB_PASS', 'Kallen.741');
+define('DB_NAME', 'b15_39936194_puntoventa'); // Cambia 'tu_base_de_datos' por el nombre de tu base de datos
 
 function getConnection() {
     $conexion = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
